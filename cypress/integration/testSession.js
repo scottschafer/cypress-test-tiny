3a776807d57029a6d4068e0d12846c29d7b0d34d
Scott Schafer <scott.schafer@gmail.com>
2018-03-08 10:34:23 -0800

    fix warning about promises, move data

--- cypress/integration/testSession.js
+++ cypress/integration/testSession.js
@@ -1,20 +1,18 @@
 describe('sessionTest', () => {
   
 
+  let fileContents = {};
+  let fileList = [];
+
   // load a session file and the files it refers to.
   // returns a promise that is only resolved after everything is loaded.
   
   function loadSession(srcSessionFile) {
       
-    // return a promise that is resolves only when all session files are loaded (and mocks are initialized)
-    return new Cypress.Promise((resolve, reject) => {
-
-      // a map of file names to contents
-      const fileContents = {};
-
       // read the session file (an array of file names)
-      cy.readFile( __dirname + "/data/" + srcSessionFile).then(fileList => {
-        console.log("fileList = " + JSON.stringify(fileList));
+      const sessionPath = __dirname + "/../importSession/sessions/" + srcSessionFile;
+      return cy.readFile(sessionPath).then(inFileList => {
+        fileList = inFileList;
         let remaining = fileList.length; // # of remaining files to load
         if (remaining === 0) {
           // special case for empty array
@@ -23,25 +21,23 @@ describe('sessionTest', () => {
         else {
           // iterate through array, loading each file
           fileList.forEach(fileName => {
-            console.log("fileName = " + fileName);
-            cy.readFile( __dirname + "/data/" + fileName).then(contents => {
+            cy.readFile( __dirname + "/../importSession/data/" + fileName).then(contents => {
               console.log("loaded " + fileName);
-              // we've loaded a file, so put it in our map
               fileContents[fileName] = contents;
 
               --remaining;              
               if (remaining === 0) {
 
                 // all files for the session have been loaded, we can mock the first API now
-                console.log("calling resolve");
-
-                resolve({fileList:fileList, fileContents: fileContents });
               }
             });
           });
         }
-      });
-    });
+      },
+      error => {
+        alert("error = " + JSON.stringify(error));
+      }
+    );
   }
 
 
@@ -50,10 +46,14 @@ describe('sessionTest', () => {
     //   Cypress Warning: Cypress detected that you returned a promise in a test, but also invoked one or more cy commands...
     // In the meantime, this does work
     
-     return loadSession("session.json").then((session) => {
-      expect(JSON.stringify(session.fileList)).to.eq('["1.json","2.json"]');
-      expect(JSON.stringify(session.fileContents)).to.eq('{"1.json":{"1":"test"},"2.json":{"2":"test"}}');
+    console.log("running test");
+
+    loadSession("session.json").then(() => {
+      expect(JSON.stringify(fileList)).to.eq('["1.json","2.json"]');
+      expect(JSON.stringify(fileContents)).to.eq('{"1.json":{"1":"test"},"2.json":{"2":"test"}}');
     });
+    
   });
+
 
 })
